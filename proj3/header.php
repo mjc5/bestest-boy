ce7986f8ef338c5737a20c54fce1236bb8bf4b94
birdwatching <mcoope15@villanova.edu>
2019-12-09 16:22:28 -0500

    added bootstrap for header.php

--- proj3/header.php
+++ proj3/header.php
@@ -1,3 +1,4 @@
+<!--
 <html>
 <head>
     <link type="text/css" rel="stylesheet" href="styles/style.css" media="screen">
@@ -15,3 +16,81 @@
                 </ul>
 </nav>
 </div>
+        
+
+        
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Best Dog</title>
+
+    <meta name="Finds the Best Dog" content="Best Dog">
+    <meta name="Dog" content="Best Dog">
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+  </head>
+  <body>
+
+    <div class="container">
+	<div class="row mt-5">
+		<div class="col-md-12">
+			<div class="row">
+				<div class="col-md-12">
+					<img alt="Bootstrap Image Preview" src="images/dogg.gif" class="mx-auto d-block">
+					<h3 class="text-center mt-5">
+						Which Dog is the Best Dog?
+					</h3>
+					<div class="row mt-5">
+						<div class="col-md-4 text-center mt-5">
+							<h3>
+								View All Dogs
+							</h3>
+							<p>
+								View all the current dogs in the lineup.
+							</p> 
+							<button type="button" class="btn btn-success" href="viewAllDogs.php"> 
+								Woof
+							</button>
+						</div>
+						<div class="col-md-4 text-center mt-5">
+							<h3>
+								Pick Dogs
+							</h3>
+							<p>
+								Pick the better dog between two dogs.
+							</p> 
+							<button type="button" class="btn btn-success" href="pickDog.php">
+								Bark
+							</button>
+						</div>
+						<div class="col-md-4 text-center mt-5">
+							<h3>
+								Add your own dog
+							</h3>
+							<p>
+								Add a dog to the lineup.
+							</p> 
+							<button type="button" class="btn btn-success" href="addBoy.php">
+								Dog sound
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/scripts.js"></script>
+  </body>
+</html>
