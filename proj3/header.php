8300b04ac1cc4d6df75e6d1da855d2483e138168
birdwatching <mcoope15@villanova.edu>
2019-12-09 16:36:06 -0500

    fixed header

--- proj3/header.php
+++ proj3/header.php
@@ -23,7 +23,8 @@
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -36,61 +37,58 @@
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
 
-  </head>
-  <body>
+</head>
+
+<body>
 
     <div class="container">
-	<div class="row mt-5">
-		<div class="col-md-12">
-			<div class="row">
-				<div class="col-md-12">
-					<img alt="Bootstrap Image Preview" src="images/dogg.gif" class="mx-auto d-block">
-					<h3 class="text-center mt-5">
-						Which Dog is the Best Dog?
-					</h3>
-					<div class="row mt-5">
-						<div class="col-md-4 text-center mt-5">
-							<h3>
-								View All Dogs
-							</h3>
-							<p>
-								View all the current dogs in the lineup.
-							</p> 
-							<button type="button" class="btn btn-success" href="viewAllDogs.php"> 
-								Woof
-							</button>
-						</div>
-						<div class="col-md-4 text-center mt-5">
-							<h3>
-								Pick Dogs
-							</h3>
-							<p>
-								Pick the better dog between two dogs.
-							</p> 
-							<button type="button" class="btn btn-success" href="pickDog.php">
-								Bark
-							</button>
-						</div>
-						<div class="col-md-4 text-center mt-5">
-							<h3>
-								Add your own dog
-							</h3>
-							<p>
-								Add a dog to the lineup.
-							</p> 
-							<button type="button" class="btn btn-success" href="addBoy.php">
-								Dog sound
-							</button>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+        <div class="row mt-5">
+            <div class="col-md-12">
+                <div class="row">
+                    <div class="col-md-12">
+                        <img alt="Bootstrap Image Preview" src="images/dogg.gif" class="mx-auto d-block">
+                        <h3 class="text-center mt-5">
+                            Which Dog is the Best Dog?
+                        </h3>
+                        <div class="row mt-5">
+                            <div class="col-md-4 text-center mt-5">
+                                <h3>
+                                    View All Dogs
+                                </h3>
+                                <p>
+                                    View all the current dogs in the lineup.
+                                </p>                                                                          
+                                <a class="btn btn-success" href="viewAllDogs.php" role="button">Woof</a>
+                                                                                               
+                            </div>
+                            <div class="col-md-4 text-center mt-5">
+                                <h3>
+                                    Pick Dogs
+                                </h3>
+                                <p>
+                                    Pick the better dog between two dogs.
+                                </p>                            
+                                <a class="btn btn-success" href="pickDog.php" role="button">Bark</a>
+                            </div>
+                            <div class="col-md-4 text-center mt-5">
+                                <h3>
+                                    Add your own dog
+                                </h3>
+                                <p>
+                                    Add a dog to the lineup.
+                                </p>
+                                <a class="btn btn-success" href="addBoy.php" role="button">Dog Sound</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/scripts.js"></script>
-  </body>
+</body>
+
 </html>
